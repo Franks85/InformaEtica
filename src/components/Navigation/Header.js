323c6375f94fcd7e89278fcf1d85707a95f59efe
FrancoJx <roberto.franceschini85@gmail.com>
2018-05-12 12:19:53 +0200

    template setup

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from "react";
 
-const header = () => {
-    return (
-        <div>
-            
+const Header = () => {
+  return (
+    <div>
+      <nav class="white">
+        <div class="nav-wrapper container">
+          <a id="logo-container" href="#top" class="brand-logo">
+            Logo
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="#sec1">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec2">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec3">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec4">Navbar Link</a>
+            </li>
+          </ul>
+
+          <ul id="nav-mobile" class="sidenav">
+            <li>
+              <a href="#sec1">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec2">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec3">Navbar Link</a>
+            </li>
+            <li>
+              <a href="#sec4">Navbar Link</a>
+            </li>
+          </ul>
+          <a href="#mobile" data-target="nav-mobile" class="sidenav-trigger">
+            <i class="material-icons">menu</i>
+          </a>
         </div>
-    )
-}
+      </nav>
+    </div>
+  );
+};
 
-export default header
+export default Header;
--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Slide from './landSections/slide'
+import SectionOne from './landSections/section1'
+import SlideTwo from './landSections/slide2'
+import SectionTwo from './landSections/section2'
+import SlideThree from './landSections/slide3'
 
 class Landing extends Component {
-    render () {
-        return (
-            <div>
-                <h2>Landing page</h2>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <Slide />
+        <SectionOne />
+        <SlideTwo />
+        <SectionTwo />
+        <SlideThree />
+      </div>
+    );
+  }
 }
 
-export default Landing
+export default Landing;
+
+
